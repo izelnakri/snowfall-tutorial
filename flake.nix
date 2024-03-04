ca35bf7476fba14c2496f798b9bdf46ce376124c
Izel Nakri <contact@izelnakri.com>
2024-03-04 17:39:19 +0100

    Initial snowfall directory structure and flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My snowfall tutorial flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+
+      # Configure Snowfall Lib, all of these settings are optional.
+      snowfall = {
+        # Tell Snowfall Lib to look in the `./nix/` directory for your
+        # Nix files.
+        root = ./nix;
+
+        # Choose a namespace to use for your flake's packages, library,
+        # and overlays.
+        namespace = "my-namespace";
+
+        # Add flake metadata that can be processed by tools like Snowfall Frost.
+        meta = {
+          # A slug to use in documentation when displaying things like file paths.
+          name = "my-awesome-flake";
+
+          # A title to show for your flake, typically the name.
+          title = "My Awesome Flake";
+        };
+      };
+    };
+}
---
+++ overlays/my-overlay/default.nix
@@ -0,0 +1,17 @@
+# Snowfall Lib provides access to your current Nix channels and inputs.
+#
+# Channels are named after NixPkgs instances in your flake inputs. For example,
+# with the input `nixpkgs` there will be a channel available at `channels.nixpkgs`.
+# These channels are system-specific instances of NixPkgs that can be used to quickly
+# pull packages into your overlay.
+#
+# All other arguments for this function are your flake inputs.
+{ channels, my-input, ... }:
+
+final: prev: {
+    # For example, to pull a package from unstable NixPkgs make sure you have the
+    # input `unstable = "github:nixos/nixpkgs/nixos-unstable"` in your flake.
+    inherit (channels.unstable) chromium;
+
+    my-package = my-input.packages.${prev.system}.my-package;
+}
